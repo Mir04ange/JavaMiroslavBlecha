576dd2773377b6358789ba626d0983e30e3e557e
mirinka <miroslavblecha063@gmail.com>
2025-09-03 12:41:01 +0200

    kompletne opaveny code melo by fungovat

--- First/numeroUno.java
+++ First/numeroUno.java
@@ -6,34 +6,50 @@ public class numeroUno {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Vítej v naší hře, hra spočívá o tom že musis uniknout ze hry");
-        System.out.println("Jdes do toho na nic se te vic neptam xd");
+        System.out.println("Vítej v naší hře, hra spočívá o tom že musíš uniknout ze hry");
+        System.out.println("Jdeš do toho, na nic se tě víc neptám xd");
 
         int hodnota = 0;
 
         while (hodnota != 3) {
-            // 1 nebo 2
             int k = random.nextInt(2) + 1;  
+            int volba = 0;
+            boolean validInput = false;
 
-            System.out.println("Budeš vybírat mezi 1 a 2. (1 = vlevo, 2 = vpravo)");
-            int volba = scanner.nextInt();
+            // Dokud nezada čislo mezi 1-2 tak to bude opakovat
+            while (!validInput) {
+                System.out.println("Budeš vybírat mezi 1 a 2. (1 = vlevo, 2 = vpravo)");
 
+                if (scanner.hasNextInt()) {
+                    volba = scanner.nextInt();
+                    if (volba == 1 || volba == 2) {
+                        validInput = true;
+                    } else {
+                        System.out.println("Zadej  číslo 1 nebo 2!");
+                    }
+                } else {
+                    System.out.println("Sakra zadej to cislo nebo jsi zmetek");
+                    scanner.next(); // zahodí špatný vstup
+                }
+            }
+
+            // Informace o volbě
             if (volba == 1) {
                 System.out.println("Vybral jsi vlevo");
             } else {
                 System.out.println("Vybral jsi vpravo");
             }
 
+            // Vyhodnocení
             if (volba == k) {
                 hodnota++;
                 System.out.println("Utekl jsi z místnosti!");
                 System.out.println("Jsi v místnosti číslo " + hodnota);
             } else {
-                System.out.println("Špatně vracis se na začátek.");
-
-                if (hodnota < 0 ) {
+                System.out.println("Špatně! Zůstal jsi v místnosti.");
+                if (hodnota == 2) {
                     hodnota = 0;
-                    System.out.println("Měl jsi " + hodnota + " ztratil jsi vše");
+                    System.out.println("Měl jsi 2 body, ale špatný výběr → skóre resetováno na 0!");
                 }
             }
         }
